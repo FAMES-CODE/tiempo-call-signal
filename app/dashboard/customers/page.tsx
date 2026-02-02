70bd7e35d5098ca550e71f75fb41d4656d220e3d
FAMES-CODE <74460480+FAMES-CODE@users.noreply.github.com>
2026-02-02 16:02:19 +0100

    feat: add DataTable component and update Customers page layout
    - Added a simple customers list in customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,19 @@
+import { DataTable } from '@/components/dashboard/data-table'
 import React from 'react'
 
 function Page() {
   return (
-    <div>Customer page</div>
+    <div className='p-4 space-y-6'>
+      <h1 className='text-2xl font-bold'>Customers List</h1>
+      <DataTable columns={[
+        { accessorKey: 'id', header: 'ID' },
+        { accessorKey: 'name', header: 'Name' },
+        { accessorKey: 'email', header: 'Email' },
+      ]} data={[
+        { id: 1, name: 'John Doe', email: 'xk0e0@example.com' },
+        { id: 2, name: 'Jane Smith', email: 'NtZwv@example.com' },
+      ]} />
+    </div>
   )
 }
 

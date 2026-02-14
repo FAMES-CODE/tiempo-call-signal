47c4f19ea86a3f52d16401bcb1e93561c87a0539
FAMES-CODE <74460480+FAMES-CODE@users.noreply.github.com>
2026-02-14 16:15:47 +0100

    fix: Fixed Select component from shadcn-ui with Controller from React-hook-form

--- components/dashboard/index/left-section/new-form-sheet.tsx
+++ components/dashboard/index/left-section/new-form-sheet.tsx
@@ -8,28 +8,34 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
-import { Select, SelectContent, SelectGroup, SelectTrigger, SelectValue, SelectItem } from "@/components/ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectTrigger,
+  SelectValue,
+  SelectItem,
+} from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import { FormSheetT } from "@/lib/schemas/formsheetSchema";
 import { Plus } from "lucide-react";
 import { useSession } from "next-auth/react";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
 
 function NewformSheet() {
   const session = useSession();
   const {
     register,
     handleSubmit,
+    control,
     formState: { errors },
   } = useForm<FormSheetT>();
-
   const onSubmit: SubmitHandler<FormSheetT> = (data) => {
     // status, createdById
     console.log({
-      status: "open",
-      createdById: session.data?.user.id,
       ...data,
-      
+      status: "open",
+      createdById: session.data?.user?.id ?? 0,
     });
   };
 
@@ -56,25 +62,35 @@ function NewformSheet() {
               className="w-full p-2 border rounded mb-4"
             />
             <div className="grid grid-cols-3 gap-4">
-              <Select {...register("callSim")} >
-                <SelectTrigger className="col-span-1">
-                  <SelectValue placeholder="Select call sim" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectItem value="403">403</SelectItem>
-                    <SelectItem value="503">503</SelectItem>
-                    <SelectItem value="903">903</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
+              <Controller
+                control={control}
+                name="callSim"
+                render={({ field }) => (
+                  <Select onValueChange={field.onChange}>
+                    <SelectTrigger className="col-span-1">
+                      <SelectValue placeholder="Select call sim" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectItem value="403">403</SelectItem>
+                        <SelectItem value="503">503</SelectItem>
+                        <SelectItem value="903">903</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
               <Input
                 {...register("callNumber")}
                 placeholder="Call number"
                 className="w-full col-span-2 ml-2 p-2 border rounded mb-4"
               />
             </div>
-            <Select {...register("customerId")}>
+            <Controller
+              control={control}
+              name="customerId"
+              render={({ field }) => (
+            <Select onValueChange={field.onChange}>
               <SelectTrigger className="w-full mb-4">
                 <SelectValue placeholder="Select customer" />
               </SelectTrigger>
@@ -85,6 +101,8 @@ function NewformSheet() {
                 </SelectGroup>
               </SelectContent>
             </Select>
+              )}
+            />
             <Textarea
               {...register("observation")}
               placeholder="Observation"

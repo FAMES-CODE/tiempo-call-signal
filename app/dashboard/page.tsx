fdc1fcc44b194e8d816119820eda8951f7c44ef9
FAMES-CODE <74460480+FAMES-CODE@users.noreply.github.com>
2026-02-20 23:55:53 +0100

    feat: added temporary Loading in the dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,9 +2,13 @@
 
 import SummaryCard from "@/components/dashboard/index/left-section/summary-card";
 import StatisticsCard from "@/components/dashboard/index/right-section/statistics-card";
-
+import { useSession } from "next-auth/react";
 
 function Page() {
+  const session = useSession();
+  if (session.status === "loading") {
+    return <div>Loading...</div>;
+  }
   return (
     <div className="grid grid-cols-2  gap-8">
       <SummaryCard />
